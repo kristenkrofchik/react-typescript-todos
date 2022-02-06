0ce32856d4c67a1bd54756cb33fd897eda15b0ec
Kristen Krofchik <k.krofchik@gmail.com>
2022-02-06 14:43:54 -0600

    moves notes to boost note. starting app

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,11 @@
 import React from 'react';
 import './App.css';
 
-let name: string;
-let age: number | string;
-let isStudent: boolean;
-let hobbies: string[];
-let role: [number, string];
 
-type Person = {
-  name: string;
-  age?: number;
-};
-
-let person: Person = {
-  name: 'Kristen',
-};
-
-let lotsOfPeople: Person[];
-
-function printName(name:string) {
-  console.log(name);
-};
-
-let printName: Function;
-
-
-function App() {
+const App: React.FC = () => {
   return (
     <div className="App">
-      Hello World!
+      <span className='heading'>Taskify</span>
     </div>
   );
 }
