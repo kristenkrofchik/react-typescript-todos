33424a88f5b6ae7e4c9280f8ce12483b1e7c28ef
Kristen Krofchik <k.krofchik@gmail.com>
2022-03-13 05:08:18 -0500

    added some state functionality for todo, todos

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import './styles.css';
 
-const InputField = () => {
+interface Props {
+    todo: string;
+    setTodo: React.Dispatch<React.SetStateAction<string>>;
+    handleAdd: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
     return (
-        <form className='input'>
-            <input type='input' placeholder='Enter task' className='input__box' />
+        <form className='input' onSubmit={handleAdd}>
+            <input type='input' 
+            value={todo}
+            onChange={(e)=>setTodo(e.target.value)}
+            placeholder='Enter task' className='input__box' />
             <button className='input__submit' type='submit'>Go</button>
         </form>
     )
